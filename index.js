5361557bfd30afed29e19b945e0914603ae2d96c
Rohun Vora <emailrohun@gmail.com>
2020-08-09 15:43:33 -0700

    [Codealong]

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const axios = require('axios'); 
+const axios = require('axios');
 const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,16 +9,20 @@ app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static('public'))
 
 // GET - main index of site
 app.get('/', function(req, res) {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/?limit=151';
   // Use request to call the API
   axios.get(pokemonUrl).then(response => {
+    console.log(response.data)
     let pokemon = response.data.results;
     res.render('index', { pokemon: pokemon.slice(0, 151) });
   });
 });
+
+
 
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
+  db.pokemon.create({
+    name: 'Pikachu'
+  }).then(function(poke) {
+    console.log('Created', poke.name)
+  })
+
+  db.pokemon.findAll().then(function(poke) {
+    console.log('Found: ', poke.name)
+  })
   // TODO: Get all records from the DB and render to view
   res.send('Render a page of favorites here');
 });
