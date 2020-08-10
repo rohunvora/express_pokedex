ad373c503b30059c2eacaf2816fc716c2ae656e2
Rohun Vora <emailrohun@gmail.com>
2020-08-09 17:54:23 -0700

    [Details Functionality] link the the details page to the pokemon

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,37 +1,42 @@
 var express = require('express');
-var axios = require('axios')
+var axios = require('axios');
 var router = express.Router();
-var db = require('../models')
+var db = require('../models');
 
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', async (req, res) => {
 
+  // TODO: Get all records from the DB and render to view
+  // res.render('pokemon/faves');
   try {
     let locatePoke = await db.pokemon.findAll()
-    console.log(locatePoke);
     res.render('faves', {pokemon:locatePoke})
   } catch (error) {
     res.render(error)
     }
   });
 
-  db.pokemon.create({
-    name: 'Pikachu'
-  }).then(function(poke) {
-    console.log('Created', poke.name)
-  })
-
-  db.pokemon.findAll().then(function(poke) {
-    console.log('Found: ', poke.name)
-  })
-  // TODO: Get all records from the DB and render to view
-  // res.render('pokemon/faves');
-
 // POST /pokemon - receive the name of a pokemon and add it to the database
-router.post('/', function(req, res) {
+router.post('/', async function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  await db.pokemon.findOrCreate({
+    where : {
+      name : req.body.name
+    }
+  })
+  res.redirect('/pokemon');
 });
+
+router.get('/:name', (req, res) => {
+  let pokeName = req.params.name
+  let pokemonUrl = `http://pokeapi.co/api/v2/pokemon/${pokeName}`;
+  // Use request to call the API
+  axios.get(pokemonUrl).then(response => {
+    console.log(response.data)
+    let pokemon = response.data;
+    res.render('show', {pokemon : pokemon})
+})
+})
 
 module.exports = router;
